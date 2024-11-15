042130eb906a7a0d9a4022e4baf7adcbf67d0ef2
seoyeonsw <c2c4@naver.com>
2024-11-16 02:20:23 +0900

    #1 [SETTING] 폴더 구성 및 폰트 시스템 구성

--- app/src/main/java/com/gdg/kakaobank/domain/repository/ExampleRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.hackathon.alddeul_babsang.domain.repository
-
-import com.hackathon.alddeul_babsang.domain.entity.ExampleEntity
-
-interface ExampleRepository {
-    suspend fun getUsers(page: Int): Result<List<ExampleEntity>>
-}
