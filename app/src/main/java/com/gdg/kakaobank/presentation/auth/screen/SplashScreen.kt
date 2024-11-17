2aebae349b16b9e619dcc46c047ac690fa1f0f10
hyunchin2 <hyun0325@sookmyung.ac.kr>
2024-11-17 13:50:58 +0900

    #5 [UI] 스플래시 구현

--- app/src/main/java/com/gdg/kakaobank/presentation/auth/screen/SplashScreen.kt
+++ app/src/main/java/com/gdg/kakaobank/presentation/auth/screen/SplashScreen.kt
@@ -1,13 +1,24 @@
 package com.gdg.kakaobank.presentation.auth.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.gdg.kakaobank.R
+import com.gdg.kakaobank.ui.theme.MainYellow
 import kotlinx.coroutines.delay
 
 
@@ -24,8 +35,24 @@ fun SplashScreen(navController: NavController, modifier: Modifier = Modifier) {
         }
     }
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        Text("스플래시", modifier = Modifier.align(Alignment.Center))
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = MainYellow)
+            .padding(16.dp),
+        contentAlignment = Alignment.TopCenter
+        ) {
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(top = 100.dp)
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.symbol_kakaobank),
+                contentDescription = "카카오뱅크 로고",
+                modifier = Modifier.size(75.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+        }
     }
 
 }
